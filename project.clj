c8ff7eef2009b2f98806df5a177a83cae70b24db
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-07 11:44:59 +0100

    Updated project settings

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject clj-flotr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple generator for Flotr charts."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot clj-flotr.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.6.0"]])
