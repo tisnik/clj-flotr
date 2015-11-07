3be1a1737f753d893a6882abfe4136281cbdf4ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-07 11:56:36 +0100

    Fixed project 'namespace'

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-flotr "0.1.0-SNAPSHOT"
+(defproject org.clojars.tisnik/clj-flotr "0.1.0-SNAPSHOT"
   :description "Simple generator for Flotr charts."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
