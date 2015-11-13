f736a30ddf57446b38449a3e446c87a6e8da6d94
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-13 21:46:48 +0100

    Added license header into project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,20 @@
+;
+;  (C) Copyright 2015  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (defproject org.clojars.tisnik/clj-flotr "0.1.0-SNAPSHOT"
   :description "Simple generator for Flotr charts."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tisnik/clj-flotr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [hiccup "1.0.4"]])
+
