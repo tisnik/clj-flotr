61b5f4cb1c7e20f622d058d7d2f94344332abefe
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-10 10:29:58 +0200

    Four new tests: flotr-scripts-[1-4]

--- test/clj_flotr/generator_test.clj
+++ test/clj_flotr/generator_test.clj
@@ -94,3 +94,35 @@
             [:script {:type "text/javascript" :src "bbb"}]    (script nil "bbb")
             [:script {:type "text/javascript" :src "aaabbb"}] (script "aaa" "bbb"))))
 
+(deftest test-flotr-scripts-1
+    (testing "Test the function flotr-scripts"
+        (is (= (flotr-scripts nil "flotr/lib" false)
+               '([:script {:type "text/javascript", :src "flotr/lib/lib/prototype-1.6.0.2.js"}]
+                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvas2image.js"}]
+                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvastext.js"}]
+                 [:script {:type "text/javascript", :src "flotr/lib/flotr-0.2.0-alpha.js"}])))))
+
+(deftest test-flotr-scripts-2
+    (testing "Test the function flotr-scripts"
+        (is (= (flotr-scripts "http://example.com" "flotr/lib" false)
+               '([:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/prototype-1.6.0.2.js"}]
+                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvas2image.js"}]
+                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvastext.js"}]
+                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/flotr-0.2.0-alpha.js"}])))))
+
+(deftest test-flotr-scripts-3
+    (testing "Test the function flotr-scripts"
+        (is (= (flotr-scripts nil "flotr/lib" true)
+               '([:script {:type "text/javascript", :src "flotr/lib/lib/prototype-1.6.0.2.js"}]
+                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvas2image.js"}]
+                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvastext.js"}]
+                 [:script {:type "text/javascript", :src "flotr/lib/flotr.debug-0.2.0-alpha.js"}])))))
+
+(deftest test-flotr-scripts-4
+    (testing "Test the function flotr-scripts"
+        (is (= (flotr-scripts "http://example.com" "flotr/lib" true)
+               '([:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/prototype-1.6.0.2.js"}]
+                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvas2image.js"}]
+                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvastext.js"}]
+                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/flotr.debug-0.2.0-alpha.js"}])))))
+
