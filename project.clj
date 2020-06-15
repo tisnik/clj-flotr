a0ff5ed94c468ca09f68566422b7332dd7c98460
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-15 10:11:55 +0200

    Formatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -11,12 +11,12 @@
 ;
 
 (defproject org.clojars.tisnik/clj-flotr "0.2.0-SNAPSHOT"
-    :description "Simple generator for Flotr charts."
-    :url "https://github.com/tisnik/clj-flotr"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]
-                   [hiccup "1.0.4"]]
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [lein-cloverage "1.0.6"]])
+  :description "Simple generator for Flotr charts."
+  :url "https://github.com/tisnik/clj-flotr"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hiccup "1.0.4"]]
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [lein-cloverage "1.0.6"]])
