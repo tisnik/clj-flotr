52afba5b5c3ce2aeb8033ce7cda696a2598ca020
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-15 10:22:30 +0200

    Formatted generator_test.clj by zprint

--- test/clj_flotr/generator_test.clj
+++ test/clj_flotr/generator_test.clj
@@ -15,152 +15,152 @@
             [clj-flotr.generator :refer :all]))
 
 (deftest test-line-chart-existence
-    (testing "Test if line-chart function exists"
-        (is (function? line-chart))))
+  (testing "Test if line-chart function exists"
+           (is (function? line-chart))))
 
 (deftest test-pie-chart-existence
-    (testing "Test if pie-chart function exists"
-        (is (function? pie-chart))))
+  (testing "Test if pie-chart function exists"
+           (is (function? pie-chart))))
 
 (deftest test-stacked-bars-existence
-    (testing "Test if stacked-bars function exists"
-        (is (function? stacked-bars))))
+  (testing "Test if stacked-bars function exists"
+           (is (function? stacked-bars))))
 
 (deftest test-script-existence
-    (testing "Test if script function exists"
-        (is (function? script))))
+  (testing "Test if script function exists"
+           (is (function? script))))
 
 (deftest test-flotr-scripts-existence
-    (testing "Test if flotr-scripts function exists"
-        (is (function? flotr-scripts))))
+  (testing "Test if flotr-scripts function exists"
+           (is (function? flotr-scripts))))
 
 (deftest test-bool->string-existence
-    (testing "Test if bool->string function exists"
-        (is (function? bool->string))))
+  (testing "Test if bool->string function exists"
+           (is (function? bool->string))))
 
 (deftest test-data-series-for-pie-chart-existence
-    (testing "Test if data-series-for-pie-chart function exists"
-        (is (function? data-series-for-pie-chart))))
+  (testing "Test if data-series-for-pie-chart function exists"
+           (is (function? data-series-for-pie-chart))))
 
 (deftest test-vals->js-existence
-    (testing "Test if vals->js function exists"
-        (is (function? vals->js))))
+  (testing "Test if vals->js function exists"
+           (is (function? vals->js))))
 
 (deftest test-data-series-for-line-chart-existence
-    (testing "Test if data-series-for-line-chart function exists"
-        (is (function? data-series-for-line-chart))))
+  (testing "Test if data-series-for-line-chart function exists"
+           (is (function? data-series-for-line-chart))))
 
 (deftest test-data-series-for-stacked-bars-existence
-    (testing "Test if data-series-for-stacked-bars function exists"
-        (is (function? data-series-for-stacked-bars))))
+  (testing "Test if data-series-for-stacked-bars function exists"
+           (is (function? data-series-for-stacked-bars))))
 
 (deftest test-legend-part-existence
-    (testing "Test if legend-part function exists"
-        (is (function? legend-part))))
+  (testing "Test if legend-part function exists"
+           (is (function? legend-part))))
 
 (deftest test-grid-part-existence
-    (testing "Test if grid-part function exists"
-        (is (function? grid-part))))
+  (testing "Test if grid-part function exists"
+           (is (function? grid-part))))
 
 ;
 ; Unit tests
 ;
 
 (deftest test-bool->string-1
-    (testing "Test the function bool->string"
-        (are [x y] (= x y)
-            "false" (bool->string false)
-            "false" (bool->string nil))))
+  (testing "Test the function bool->string"
+           (are [x y] (= x y)
+                "false" (bool->string false)
+                "false" (bool->string nil))))
 
 (deftest test-bool->string-2
-    (testing "Test the function bool->string"
-        (are [x y] (= x y)
-            "true" (bool->string true)
-            "true" (bool->string 42)
-            "true" (bool->string "foobar")
-            "true" (bool->string '())
-            "true" (bool->string [])
-            "true" (bool->string {})
-            "true" (bool->string #{}))))
+  (testing "Test the function bool->string"
+           (are [x y] (= x y)
+                "true" (bool->string true)
+                "true" (bool->string 42)
+                "true" (bool->string "foobar")
+                "true" (bool->string '())
+                "true" (bool->string [])
+                "true" (bool->string {})
+                "true" (bool->string #{}))))
 
 (deftest test-script
-    (testing "Test the function script"
-        (are [x y] (= x y)
-            [:script {:type "text/javascript" :src ""}]       (script nil nil)
-            [:script {:type "text/javascript" :src ""}]       (script "" "")
-            [:script {:type "text/javascript" :src "aaa"}]    (script "aaa" "")
-            [:script {:type "text/javascript" :src "bbb"}]    (script "" "bbb")
-            [:script {:type "text/javascript" :src "aaa"}]    (script "aaa" nil)
-            [:script {:type "text/javascript" :src "bbb"}]    (script nil "bbb")
-            [:script {:type "text/javascript" :src "aaabbb"}] (script "aaa" "bbb"))))
+  (testing "Test the function script"
+           (are [x y] (= x y)
+                [:script {:type "text/javascript" :src ""}]       (script nil nil)
+                [:script {:type "text/javascript" :src ""}]       (script "" "")
+                [:script {:type "text/javascript" :src "aaa"}]    (script "aaa" "")
+                [:script {:type "text/javascript" :src "bbb"}]    (script "" "bbb")
+                [:script {:type "text/javascript" :src "aaa"}]    (script "aaa" nil)
+                [:script {:type "text/javascript" :src "bbb"}]    (script nil "bbb")
+                [:script {:type "text/javascript" :src "aaabbb"}] (script "aaa" "bbb"))))
 
 (deftest test-flotr-scripts-1
-    (testing "Test the function flotr-scripts"
-        (is (= (flotr-scripts nil "flotr/lib" false)
-               '([:script {:type "text/javascript", :src "flotr/lib/lib/prototype-1.6.0.2.js"}]
-                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvas2image.js"}]
-                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvastext.js"}]
-                 [:script {:type "text/javascript", :src "flotr/lib/flotr-0.2.0-alpha.js"}])))))
+  (testing "Test the function flotr-scripts"
+           (is (= (flotr-scripts nil "flotr/lib" false)
+                  '([:script {:type "text/javascript", :src "flotr/lib/lib/prototype-1.6.0.2.js"}]
+                    [:script {:type "text/javascript", :src "flotr/lib/lib/canvas2image.js"}]
+                    [:script {:type "text/javascript", :src "flotr/lib/lib/canvastext.js"}]
+                    [:script {:type "text/javascript", :src "flotr/lib/flotr-0.2.0-alpha.js"}])))))
 
 (deftest test-flotr-scripts-2
-    (testing "Test the function flotr-scripts"
-        (is (= (flotr-scripts "http://example.com" "flotr/lib" false)
-               '([:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/prototype-1.6.0.2.js"}]
-                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvas2image.js"}]
-                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvastext.js"}]
-                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/flotr-0.2.0-alpha.js"}])))))
+  (testing "Test the function flotr-scripts"
+           (is (= (flotr-scripts "http://example.com" "flotr/lib" false)
+                  '([:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/prototype-1.6.0.2.js"}]
+                    [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvas2image.js"}]
+                    [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvastext.js"}]
+                    [:script {:type "text/javascript", :src "http://example.com/flotr/lib/flotr-0.2.0-alpha.js"}])))))
 
 (deftest test-flotr-scripts-3
-    (testing "Test the function flotr-scripts"
-        (is (= (flotr-scripts nil "flotr/lib" true)
-               '([:script {:type "text/javascript", :src "flotr/lib/lib/prototype-1.6.0.2.js"}]
-                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvas2image.js"}]
-                 [:script {:type "text/javascript", :src "flotr/lib/lib/canvastext.js"}]
-                 [:script {:type "text/javascript", :src "flotr/lib/flotr.debug-0.2.0-alpha.js"}])))))
+  (testing "Test the function flotr-scripts"
+           (is (= (flotr-scripts nil "flotr/lib" true)
+                  '([:script {:type "text/javascript", :src "flotr/lib/lib/prototype-1.6.0.2.js"}]
+                    [:script {:type "text/javascript", :src "flotr/lib/lib/canvas2image.js"}]
+                    [:script {:type "text/javascript", :src "flotr/lib/lib/canvastext.js"}]
+                    [:script {:type "text/javascript", :src "flotr/lib/flotr.debug-0.2.0-alpha.js"}])))))
 
 (deftest test-flotr-scripts-4
-    (testing "Test the function flotr-scripts"
-        (is (= (flotr-scripts "http://example.com" "flotr/lib" true)
-               '([:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/prototype-1.6.0.2.js"}]
-                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvas2image.js"}]
-                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvastext.js"}]
-                 [:script {:type "text/javascript", :src "http://example.com/flotr/lib/flotr.debug-0.2.0-alpha.js"}])))))
+  (testing "Test the function flotr-scripts"
+           (is (= (flotr-scripts "http://example.com" "flotr/lib" true)
+                  '([:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/prototype-1.6.0.2.js"}]
+                    [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvas2image.js"}]
+                    [:script {:type "text/javascript", :src "http://example.com/flotr/lib/lib/canvastext.js"}]
+                    [:script {:type "text/javascript", :src "http://example.com/flotr/lib/flotr.debug-0.2.0-alpha.js"}])))))
 
 (deftest test-data-series-for-pie-chart-1
-    (testing "Test the function data-series-for-pie-chart"
-        (are [x y] (= x y)
-            (data-series-for-pie-chart nil) '()
-            (data-series-for-pie-chart [])  '())))
+  (testing "Test the function data-series-for-pie-chart"
+           (are [x y] (= x y)
+                (data-series-for-pie-chart nil) '()
+                (data-series-for-pie-chart [])  '())))
 
 (deftest test-data-series-for-pie-chart-2
-    (testing "Test the function data-series-for-pie-chart"
-        (are [x y] (= x y)
-            (data-series-for-pie-chart [{:values :v1 :label :l1}])
-                                       ["{data:[[1,:v1]], label: ':l1'},\n"]
-            (data-series-for-pie-chart [{:values :v1 :label :l1}
-                                        {:values :v2 :label :l2}])
-                                       ["{data:[[1,:v1]], label: ':l1'},\n"
-                                        "{data:[[1,:v2]], label: ':l2'},\n"])))
+  (testing "Test the function data-series-for-pie-chart"
+           (are [x y] (= x y)
+                (data-series-for-pie-chart [{:values :v1 :label :l1}])
+                                           ["{data:[[1,:v1]], label: ':l1'},\n"]
+                (data-series-for-pie-chart [{:values :v1 :label :l1}
+                                            {:values :v2 :label :l2}])
+                                           ["{data:[[1,:v1]], label: ':l1'},\n"
+                                            "{data:[[1,:v2]], label: ':l2'},\n"])))
 
 (deftest test-data-series-for-pie-chart-3
-    (testing "Test the function data-series-for-pie-chart"
-        (are [x y] (= x y)
-            (data-series-for-pie-chart [{:values nil :label nil}])
-                                       ["{data:[[1,]], label: ''},\n"]
-            (data-series-for-pie-chart [{:values nil :label :l1}])
-                                       ["{data:[[1,]], label: ':l1'},\n"]
-            (data-series-for-pie-chart [{:values :v1 :label nil}])
-                                       ["{data:[[1,:v1]], label: ''},\n"])))
+  (testing "Test the function data-series-for-pie-chart"
+           (are [x y] (= x y)
+                (data-series-for-pie-chart [{:values nil :label nil}])
+                                           ["{data:[[1,]], label: ''},\n"]
+                (data-series-for-pie-chart [{:values nil :label :l1}])
+                                           ["{data:[[1,]], label: ':l1'},\n"]
+                (data-series-for-pie-chart [{:values :v1 :label nil}])
+                                           ["{data:[[1,:v1]], label: ''},\n"])))
 
 (deftest test-data-series-for-line-chart-1
-    (testing "Test the function data-series-for-line-chart"
-        (are [x y] (= x y)
-            (data-series-for-line-chart nil) '()
-            (data-series-for-line-chart [])  '())))
+  (testing "Test the function data-series-for-line-chart"
+           (are [x y] (= x y)
+                (data-series-for-line-chart nil) '()
+                (data-series-for-line-chart [])  '())))
 
 (deftest test-data-series-for-stacked-bars-1
-    (testing "Test the function data-series-for-stacked-bars"
-        (are [x y] (= x y)
-            (data-series-for-stacked-bars nil) '()
-            (data-series-for-stacked-bars [])  '())))
+  (testing "Test the function data-series-for-stacked-bars"
+           (are [x y] (= x y)
+                (data-series-for-stacked-bars nil) '()
+                (data-series-for-stacked-bars [])  '())))
 
